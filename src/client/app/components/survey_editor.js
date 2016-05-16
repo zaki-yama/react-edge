590abfcceca58b613dc2331907f586110f017bd6
zaki-yama <shingoyamazaki00@gmail.com>
2016-05-16 20:06:04 +0900

    5.2.1

--- src/client/app/components/survey_editor.js
+++ src/client/app/components/survey_editor.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 export default class SurveyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropZoneEntered: false,
+      title: '',
+      introduction: '',
+      questions: []
+    };
+  }
+
   render() {
+    const questions = this.state.questions;
+
+    let dropZoneEntered = '';
+    if (this.state.dropZoneEntered) {
+      dropZoneEntered = 'drag-enter';
+    }
+
     return (
       <div className='survey-editor'>
         <div className='row'>
@@ -11,14 +28,27 @@ export default class SurveyEditor extends React.Component {
           </aside>
 
           <div className='survey-canvas col-md-9'>
+            <SurveyForm
+              title={this.state.title}
+              introduction={this.state.introduction}
+              onChange={this.handleFormChange}
+            />
+
+            <Divider>質問</Divider>
+
             <div
-              className={'drop-zone well well-drop-zone'}
+              className={'drop-zone well well-drop-zone ' + dropZoneEntered}
               onDragOver={this.handleDragOver}
               onDragEnter={this.handleDragEnter}
               onDragLeave={this.handleDragLeave}
               onDrop={this.handleDrop}
             >
               左側の部品をドラッグアンドドロップしてください
+            </div>
+
+            <div className='actions'>
+              <button className="btn btn-save"
+                onClick={this.handleSaveClicked}>保存</button>
             </div>
           </div>
         </div>
