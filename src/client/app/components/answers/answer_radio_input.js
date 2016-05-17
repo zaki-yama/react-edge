2478fdc4fb0940426ec7ae95a0d304a9e81f6802
zaki-yama <shingoyamazaki00@gmail.com>
2016-05-17 19:49:01 +0900

    6.2.2, 6.2.3

--- src/client/app/components/answers/answer_radio_input.js
+++ src/client/app/components/answers/answer_radio_input.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
 export default class AnswerRadioInput extends React.Component {
+  constructor(props) {
+    this.state = {
+      checked: !!this.props.checked,
+      id: this.props.id ? this.props.id : uniqueId('radio-')
+    };
+  }
+
   render() {
     return (
       <div className="radio">
-        <label>
-          <input type="radio" />
-          ラベルの文字列
+        <label htmlFor={this.state.id}>
+          <input type="radio"
+            name={this.props.name}
+            id={this.state.id}
+            value={this.props.value}
+            checked={this.state.checked} />
+          {this.props.label}
         </label>
       </div>
     );
   }
 }
+
+AnswerRadioInput.propTypes = {
+  id: React.PropTypes.string,
+  name: React.PropTypes.string.isRequired,
+  label: React.PropTypes.string.isRequired,
+  value: React.PropTypes.string.isRequired,
+  checked: React.PropTypes.bool
+};
+
+AnswerRadioInput.defaultProps = {
+  id: null,
+  checked: false
+};
