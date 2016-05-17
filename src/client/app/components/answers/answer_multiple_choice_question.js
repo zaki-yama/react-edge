b7dc4ee7aeed3f21588dc10ef532c47dc2e643e8
zaki-yama <shingoyamazaki00@gmail.com>
2016-05-18 00:37:54 +0900

    6.2.4

--- src/client/app/components/answers/answer_multiple_choice_question.js
+++ src/client/app/components/answers/answer_multiple_choice_question.js
@@ -1,6 +1,38 @@
 import React from 'react';
 
 export default class AnswerMultipleChoiceQuestion extends React.Component {
+  construcor(props) {
+    this.state = {
+      id: uniqueId('multiple-choice-'),
+      value: this.props.value
+    };
+  }
+
+  renderChoices() {
+    return this.props.choices.map((choice, i) => {
+      return (
+        <AnswerRadioInput
+          key={"choice-" + i}
+          name={this.state.id}
+          label={choice}
+          value={choice}
+          checked={this.state.value === choice} />
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="form-group">
+        <label className="survey-item-label" htmlFor={this.state.id}>
+          {this.props.label}
+        </label>
+        <div className="survey-item-content">
+          {this.renderChoices()}
+        </div>
+      </div>
+    );
+  }
 }
 
 AnswerMultipleChoiceQuestion.propTypes = {
@@ -9,5 +41,3 @@ AnswerMultipleChoiceQuestion.propTypes = {
   onCompleted: React.PropTypes.func.isRequired
 };
 
-AnswerMultipleChoiceQuestion.defaultProps = {
-  value: 
