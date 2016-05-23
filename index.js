e7c3208e956349f820f70bfd00e406e33f4e891c
zaki-yama <shingoyamazaki00@gmail.com>
2016-05-23 20:05:32 +0900

    9.2 管理されたコンポーネント

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
@@ -22,6 +23,44 @@ class MyForm extends React.Component {
           ref="helloTo"
           type="text"
           defaultValue="Hello World!"
+        />
+        <button type="submit">送信</button>
+      </form>
+    );
+  }
+}
+*/
+
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      helloTo: 'Hello World!'
+    };
+
+    this.submitHandler = this.submitHandler.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      helloTo: e.target.value.toUpperCase()
+    });
+  }
+
+  submitHandler(e) {
+    e.preventDefault();
+    alert(this.state.helloTo);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.submitHandler}>
+        <input
+          type="text"
+          value={this.state.helloTo}
+          onChange={this.handleChange}
         />
         <button type="submit">送信</button>
       </form>
